aa2f43a01af4ba5300cf971dd451912940e15f23
anishfall2017husky <surti.a@husky.neu.edu>
2017-12-05 05:40:26 -0500

    Added Node, Express and Mongoose coode - mongo connction still left

---
+++ SocialLearningPlatform/assets/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseListComponent } from './courses/course-list/course-list.component';
+import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { CourseItemComponent } from './courses/course-list/course-item/course-item.component';
+import {DropdownDirective} from './courses/Shared/dropdown.directive';
+import {AppRoutingModule} from './app-routing.module';
+import { CourseStartComponent } from './courses/course-start/course-start.component';
+import { CourseEditComponent } from './courses/course-edit/course-edit.component';
+import {CourseService} from './courses/course.service';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        CoursesComponent,
+        CourseListComponent,
+        CourseDetailComponent,
+        CourseItemComponent,
+        DropdownDirective,
+        CourseStartComponent,
+        CourseEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpModule
+    ],
+    providers: [CourseService],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
